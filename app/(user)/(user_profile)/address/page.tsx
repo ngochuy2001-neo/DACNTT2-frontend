a8aac9b7397118219ce096706e39c2816aa0da51
Nguyen Le Ngoc Huy <71955356+ngochuy2001-neo@users.noreply.github.com>
2025-02-03 21:12:27 +0700

    Add page address

--- app/(user)/(user_profile)/address/page.tsx
+++ app/(user)/(user_profile)/address/page.tsx
@@ -1,7 +1,22 @@
+import { Typography } from "@mui/material";
 import React from "react";
 
 function AddressPage() {
-  return <div>AddressPage</div>;
+  return (
+    <div className="w-[100%]">
+      <div>
+        <Typography
+          sx={{
+            fontSize: "20px",
+            fontWeight: "bold",
+          }}
+        >
+          Quản lý địa chỉ nhận hàng
+        </Typography>
+      </div>
+      <div className="flex bg-white w-[100%] p-[20px] rounded-md"></div>
+    </div>
+  );
 }
 
 export default AddressPage;
